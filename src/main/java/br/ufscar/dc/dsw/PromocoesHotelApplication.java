976bb1252a14704190b739d1231ff83d2d250e09
KenzoInanami <kenzoinanami@gmail.com>
2023-07-17 15:07:21 -0300

    Projeto criado pelo Spring Initializr

---
+++ src/main/java/br/ufscar/dc/dsw/PromocoesHotelApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocoesHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocoesHotelApplication.class, args);
+	}
+
+}
